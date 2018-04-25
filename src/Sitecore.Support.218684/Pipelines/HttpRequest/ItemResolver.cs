ffe9e3adfe74d2d62330ca779f931bd75850d4ab
Kirill Volovitskiy <kiv@sitecore.net>
2018-04-25 10:49:57 +0300

    Default Code

---
+++ src/Sitecore.Support.218684/Pipelines/HttpRequest/ItemResolver.cs
@@ -0,0 +1,108 @@
+﻿namespace Sitecore.Support.Pipelines.HttpRequest
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using Sitecore.Abstractions;
+  using Sitecore.Configuration;
+  using Sitecore.Data.ItemResolvers;
+  using Sitecore.Data.Items;
+  using Sitecore.DependencyInjection;
+  using Sitecore.Diagnostics;
+  using Sitecore.IO;
+  using Sitecore.SecurityModel;
+  using Sitecore.Sites;
+  using Sitecore.StringExtensions;
+  using Sitecore.Web;
+  using Sitecore.Pipelines.HttpRequest;
+
+  public class ItemResolver : Sitecore.Pipelines.HttpRequest.ItemResolver
+  {
+    #region Constructors
+    [Obsolete("Please use another constructor with parameters")]
+    public ItemResolver() : base()
+    {
+    }
+
+    public ItemResolver(BaseItemManager itemManager, ItemPathResolver pathResolver) : base(itemManager, pathResolver)
+    {
+    }
+
+    protected ItemResolver([NotNull] BaseItemManager itemManager, [NotNull] ItemPathResolver pathResolver, MixedItemNameResolvingMode itemNameResolvingMode) :
+      base (itemManager, pathResolver, itemNameResolvingMode)
+    {
+    }
+    #endregion
+
+    public override void Process([NotNull] HttpRequestArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+
+      if (this.SkipItemResolving(args))
+      {
+        return;
+      }
+
+      bool permissionDenied = false;
+      Item resolvedItem = null;
+
+      string path = string.Empty;
+
+      try
+      {
+        this.StartProfilingOperation("Resolve current item.", args);
+
+        var uniquePaths = new HashSet<string>();
+        foreach (var candidatePath in this.GetCandidatePaths(args))
+        {
+          if (!uniquePaths.Add(candidatePath))
+          {
+            continue; // Already checked this URL
+          }
+
+          if (this.TryResolveItem(candidatePath, args, out resolvedItem, out permissionDenied))
+          {
+            path = candidatePath; // found matching item by path, will stop search
+            break;
+          }
+
+          if (permissionDenied)
+          {
+            return; // found item exists, but we cannot touch it due to lack of permissions.
+          }
+        }
+
+        var site = Sitecore.Context.Site;
+
+        if (resolvedItem == null || resolvedItem.Name.Equals("*"))
+        {
+          var displayNameItem = this.ResolveByMixedDisplayName(args, out permissionDenied);
+          if (displayNameItem != null)
+          {
+            resolvedItem = displayNameItem;
+          }
+        }
+
+        if (resolvedItem == null && site != null && !permissionDenied && this.UseSiteStartPath(args))
+        {
+          if (this.TryResolveItem(site.StartPath, args, out resolvedItem, out permissionDenied))
+          {
+            path = site.StartPath;
+          }
+        }
+      }
+      finally
+      {
+        if (resolvedItem != null)
+        {
+          this.TraceInfo("Current item is {0}.".FormatWith(path));
+        }
+
+        args.PermissionDenied = permissionDenied;
+        Sitecore.Context.Item = resolvedItem;
+
+        this.EndProfilingOperation(status: null, args: args);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.218684/Pipelines/ItemProvider/GetItem/EnforceVersionPresence.cs
@@ -0,0 +1,34 @@
+﻿namespace Sitecore.Support.Pipelines.ItemProvider.GetItem
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Pipelines.ItemProvider.GetItem;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  public class EnforceVersionPresence : Sitecore.Pipelines.ItemProvider.GetItem.EnforceVersionPresence
+  {
+    public override void Process(GetItemArgs args)
+    {
+      if (!this.IsEnforceVersionPresenceEnabled())
+      {
+        return;
+      }
+
+      var item = this.GetItem(args);
+
+      args.Result = this.IsItemEnforceVersionPresenceEnabled(item) ? null : item;
+    }
+    private Item GetItem(GetItemArgs args)
+    {
+      if (args == null)
+      {
+        return null;
+      }
+
+      return args.Result != null || args.Handled ? args.Result : ((object)args.ItemId != null
+           ? args.FallbackProvider.GetItem(args.ItemId, args.Language, args.Version, args.Database, args.SecurityCheck)
+           : args.FallbackProvider.GetItem(args.ItemPath, args.Language, args.Version, args.Database, args.SecurityCheck));
+    }
+  }
+}
